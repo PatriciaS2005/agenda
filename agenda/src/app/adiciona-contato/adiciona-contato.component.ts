a1d9112813a856e57c43610c5e54422db733bf9c
Patricia Santana <140630121+PatriciaS2005@users.noreply.github.com>
2024-10-28 23:29:11 -0300

    fe em deus

--- agenda/src/app/adiciona-contato/adiciona-contato.component.ts
+++ agenda/src/app/adiciona-contato/adiciona-contato.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-adiciona-contato',
   templateUrl: './adiciona-contato.component.html',
-  styleUrl: './adiciona-contato.component.css'
+  
 })
 export class AdicionaContatoComponent {
 
